a53fa3011179d337cb612ad4092f8f90ec3eca68
Oleksandr P <goodwin009@gmail.com>
2022-10-08 15:35:40 +0300

    add result by 3 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
 
+//персональні налаштування плеєру
+player.setVolume(0); // зі старту вимкнений звук відео
+player.setColor('#48ef00'); // замінив на зелений колір
+
+// отримуємо поточний час переглянутого відео в секундах та зберігаємо до localStorage
+const onTimeUpdate = function ({ seconds }) {
+  if (seconds) {
+    localStorage.setItem(STORAGE_KEY, seconds);
+  }
+};
+
+// отримуємо збережий час з localStorage та передаємо до плеєру перед запуском перегляду
+const onStartFromPrevTime = function () {
+  const currentTime = localStorage.getItem(STORAGE_KEY);
+  player.setCurrentTime(currentTime);
+};
+
+//додаємо слухача подій плеєра та кожні 1000мс отримуємо поточне значення в секундах переглянутого відео
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+//додаємо слухача на старт програвання відео та додаємо значення в секундахб де був зупинений перегляд останнього сеансу
+player.on('play', onStartFromPrevTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,61 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
 
+//беремо посилання на форму
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+//формуємо пустий обєкт даних для подальшого наповнення з форми
+const formData = {};
+
+//додаємо слухача на введення та затримку на фіксацію змін в 500мс
+refs.form.addEventListener('input', throttle(onLocalStorageSave, 500));
+
+//додаємо слухача на натиснення кнопки submit
+refs.form.addEventListener('submit', onFormSubmit);
+
+//перевіряємо чи є дані від попереднього введення у форму та якщо є - прописуємо збережені дані у форму
+// getStorageKey();
+
+//функція формує обєкт із введених даних у формі згідно назви елементів форми та перетворює в строку і зберігає в localStorage
+function onLocalStorageSave(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+//функція при натисненні на кнопку submit: відключаємо подію перезавантаження сторінки, очищуємо поля поточної форми, формуємо обєкт із даних в localStorage, очщаємо localStorage по ключу STORAGE_KEY та виводимо обєкт в консоль
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+
+  let userData = '';
+  try {
+    userData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  } catch {
+    console.log(error.name);
+    console.log(error.message);
+  }
+
+  localStorage.removeItem(STORAGE_KEY);
+  return console.log(userData);
+}
+
+//перевіряємо чи є дані від попереднього введення у форму та якщо є - прописуємо збережені дані у форму, виклик функції в місці оголошення
+// getStorageKey();
+(function getStorageKey(e) {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    let userData = {};
+
+    try {
+      userData = JSON.parse(savedData);
+    } catch {
+      console.log(error.name);
+      console.log(error.message);
+    }
+
+    refs.form.message.value = userData.message;
+    refs.form.email.value = userData.email;
+  }
+})();
